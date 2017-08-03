30e3adbebcd9c13f719e4af39e9c7dd6089e575c
Kieve Chua <kievechua@gmail.com>
2017-08-03 10:45:12 +0800

    update core

---
+++ dist/config/bugsnag.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// https://docs.bugsnag.com/platforms/nodejs/koa/
+exports.default = {
+    apiKey: '',
+};
+//# sourceMappingURL=bugsnag.js.map
